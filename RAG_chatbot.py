6e6aff0452651f14e308f8ae36e700c302a35b39
switin2006 <switinmechineni@gmail.com>
2025-02-04 11:00:27 +0530

    Update RAG_chatbot.py

--- RAG_chatbot.py
+++ RAG_chatbot.py
@@ -75,7 +75,7 @@ if uploaded_file:
                       
                       temp_file.write(Audio_record.read())
                 
-                transcription =whisper.load_model("base").transcribe(temp_file_path)
+                transcription =whisper.load_model("medium").transcribe(temp_file_path,,language="en")
                 query=transcription["text"]
                 st.write(query)
                 retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
